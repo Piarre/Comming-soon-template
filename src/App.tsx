eac06789e1607399c2d669745257244c9f686d8a
Piarre <pierre@ideestore.fr>
2023-02-28 18:19:44 +0100

    ini

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Comming soon !</h1>
+      <footer className="footer" style={{ fontFamily: "Inter, system-ui" }}>
+        <p>{new Date().getFullYear()} IDE Pierre</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
